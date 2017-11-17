a2204c89bbd230279adeed8b915f563c1d79892a
thien ban BUI <thienban91@hotmail.fr>
2017-11-17 10:37:20 +0100

    exemple Lamda

---
+++ java/ex01/TestLamda.java
@@ -0,0 +1,32 @@
+package ex01;
+
+import java.util.Comparator;
+
+import java8.data.domain.Pizza;
+
+public class TestLamda {
+
+	public static void main(String[] args) {
+		
+		Comparator<Pizza> c = new Comparator<Pizza>() {
+
+			@Override
+			public int compare(Pizza o1, Pizza o2) {
+				return o1.getPrice().compareTo(o2.getPrice());
+			}
+		};
+		
+		Comparator<Pizza> c2 = (Pizza o1, Pizza o2) -> {
+				
+				return o1.getPrice().compareTo(o2.getPrice());
+		};
+		
+		Comparator<Pizza> c3 = (Pizza o1, Pizza o2) ->
+			
+			 o1.getPrice().compareTo(o2.getPrice());
+			 
+		Comparator<Pizza> c4 = ( o1, o2) ->
+			
+				 o1.getPrice().compareTo(o2.getPrice());
+	}
+}
